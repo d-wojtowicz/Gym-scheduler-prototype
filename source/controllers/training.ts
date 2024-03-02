1394387aafe49dc0443a16144553e1ed5f60056e
Gruby3773 <dwojtowicz1@o2.pl>
2024-03-02 19:30:37 +0100

    add: new GET endpoint (getTrainingByDate)

--- source/controllers/training.ts
+++ source/controllers/training.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import mongoose from 'mongoose';
 import Training from '../models/training';
 
-
+// POST
 const createTraining = (req: Request, res: Response, next: NextFunction) => {
     let { date, workoutType, workoutPlan, extraInformation } = req.body;
 
@@ -29,6 +29,7 @@ const createTraining = (req: Request, res: Response, next: NextFunction) => {
     });
 };
 
+// GET
 const getAllTrainings = (req: Request, res: Response, next: NextFunction) => {
     Training.find()
     .exec()
@@ -45,5 +46,41 @@ const getAllTrainings = (req: Request, res: Response, next: NextFunction) => {
         });
     });
 };
+const getTrainingByDate = (req: Request, res: Response, next: NextFunction) => {
+    const trainingsDate = req.params.date;
 
-export default { createTraining, getAllTrainings };
+    if (!trainingsDate) {
+        return res.status(400).json({
+            message: "No date provided."
+        });
+    }
+
+    const startOfDay = new Date(trainingsDate).setUTCHours(0, 0, 0, 0);
+    const endOfDay = new Date(trainingsDate).setUTCHours(23, 59, 59, 999);
+    console.log(startOfDay, endOfDay)
+    Training.find({
+        date: {
+            $gte: startOfDay,
+            $lte: endOfDay
+        }
+    })
+    .exec()
+    .then((results) => {
+        return res.status(200).json({
+            trainings: results,
+            count: results.length
+        });
+    })
+    .catch((error) => {
+        return res.status(500).json({
+            message: error.message,
+            error
+        })
+    })
+};
+
+export default { 
+    createTraining, 
+    getAllTrainings, 
+    getTrainingByDate 
+};
--- source/routes/training.ts
+++ source/routes/training.ts
@@ -4,6 +4,8 @@ import controller from '../controllers/training';
 const router = express.Router();
 
 router.post('/create/training', controller.createTraining);
+
 router.get('/get/trainings', controller.getAllTrainings);
+router.get('/get/trainings/date/:date', controller.getTrainingByDate);
 
 export = router;
