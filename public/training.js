8b17e5132db995fd41cd8e76e267682107d9cbc5
Gruby3773 <dwojtowicz1@o2.pl>
2024-03-21 16:36:41 +0100

    add: initial css and training endpoint

--- public/training.js
+++ public/training.js
@@ -4,27 +4,33 @@ document.addEventListener("DOMContentLoaded", function() {
 
     fetch(`/api/get/trainings/date/${trainingDate}`)
     .then(response => response.json())
-    .then(trainings => {
-        document.getElementById("trainingDate").innerText = formatDate(trainings.trainings[0].date);
+    .then(data => {
+        // Create 'date of training'
+        document.getElementById("trainingDate").innerText = formatDate(data.trainings[0].date);
 
         const listTrainingsPerDay = document.getElementById("trainingsListPerDay");
-        trainings.trainings.forEach(training => {
+        data.trainings.forEach(training => {
+            // Create 'main div' for all gathered info
             const trainingBlock = document.createElement("div");
             trainingBlock.setAttribute("id", training._id);
 
+            // Create 'workout type header' inside 'main div'
             const workoutType = document.createElement("h2");
-            const workoutPlan = document.createElement("p");
             workoutType.innerText = training.workoutType;
-            workoutPlan.innerText = training.workoutPlan;
-
             trainingBlock.appendChild(workoutType);
+            
+            // Create 'workout plan table' inside 'main div'
+            const workoutPlan = formatTable(training.workoutPlan);
             trainingBlock.appendChild(workoutPlan);
-            trainingBlock.appendChild(document.createElement("br"));
 
+            // Append generated elements
+            trainingBlock.appendChild(document.createElement("br"));
             listTrainingsPerDay.appendChild(trainingBlock);
         });
     });
 });
+
+
 
 function formatDate(date){
     const d = new Date(date),
@@ -33,4 +39,49 @@ function formatDate(date){
     year = d.getFullYear();
 
     return [year, month.padStart(2, '0'), day.padStart(2, '0')].join('-');
+}
+
+function formatTable(workoutPlan){
+    // Init 'main table' and 'header row'
+    const singleTrainingTable = document.createElement("table");
+    singleTrainingTable.setAttribute("class", "trainingTable")
+    const headerRow = document.createElement("tr");
+    const headers = ["Exercise", "Weight load", "Sets", "Repetitions", "Notes"];
+
+    // Fill the 'header row' for 'main table'
+    headers.forEach(headerText => {
+        const header = document.createElement("th");
+        header.innerText = headerText;
+        headerRow.appendChild(header);
+    });
+    singleTrainingTable.appendChild(headerRow);
+
+    workoutPlan.forEach(exercise => {
+        const row = document.createElement("tr");
+
+        // Create cells for 'single exercise row'
+        const typeCell = document.createElement("td");
+        const weightLoadCell = document.createElement("td");
+        const sets = document.createElement("td");
+        const repetitions = document.createElement("td");
+        const notes = document.createElement("td");
+
+        // Fill in generated cells
+        typeCell.innerText = exercise.type;
+        weightLoadCell.innerText = exercise.weight_load;
+        sets.innerText = exercise.sets;
+        repetitions.innerText = exercise.repetitions;
+        notes.innerText = exercise.notes || "-";
+        
+        // Merge prepared cells into single 'exercise row'
+        row.appendChild(typeCell);
+        row.appendChild(weightLoadCell);
+        row.appendChild(sets);
+        row.appendChild(repetitions);
+        row.appendChild(notes);
+
+        singleTrainingTable.appendChild(row);
+    });
+
+    return singleTrainingTable;
 }
