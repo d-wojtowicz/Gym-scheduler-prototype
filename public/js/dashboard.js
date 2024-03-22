a1f3d1ae14465676f7f3af54ee1c8d001d430ce8
Gruby3773 <dwojtowicz1@o2.pl>
2024-03-22 20:06:20 +0100

    add: content loaders

---
+++ public/js/dashboard.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const token = localStorage.getItem('token');
+    if (!token) {
+        window.location.href = '/';
+        return;
+    }
+
+    fetch('/api/get/trainings', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',
+        },
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Create training assignments to appropriate usersId
+    })
+});
---
+++ public/js/login.js
@@ -0,0 +1,47 @@
+// Token reset
+if (localStorage.getItem('token')) {
+    localStorage.removeItem('token');
+}
+
+document.getElementById('loginForm').addEventListener('submit', function(e){
+    e.preventDefault();
+
+    // Fetch filled 'username' and 'password' from 'Login form'
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    // Try to generate 'token' and log in to the 'dashboard'
+    fetch('/api/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({username, password})
+    })
+    .then(response => response.json())
+    .then(data => {
+        // If token is generated (successful)
+        if (data.token) {
+            // Try to open the training panel ('dashboard')
+            localStorage.setItem('token', data.token);
+            fetch('/dashboard', {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${data.token}`
+                }
+            })
+            .then(response => {
+                if (response.ok){
+                    // This does not look correct, take a look at it later
+                    window.location.href = "/dashboard"; 
+                }
+            })
+        }
+        // If token is NOT generated (unsuccessful)
+        if (data.message) {
+            const error_msg = document.createElement("p");
+            error_msg.innerText = data.message;
+            document.getElementById('loginResponse').replaceChildren(error_msg);
+        }
+    });
+});
