cff72195e3724d01f72fda0fd40694c7dc06ef4b
Gruby3773 <dwojtowicz1@o2.pl>
2024-04-20 13:30:13 +0200

    update: add exercise modal

--- public/js/training.js
+++ public/js/training.js
@@ -1,3 +1,9 @@
+const workoutPlanTableHeaders = {
+    'name': "Name", 
+    'weightLoad': "Weight load", 
+    'sets': "Sets", 
+    'repetitions': "Repetitions"
+};
 document.addEventListener('DOMContentLoaded', function() {
     const token = localStorage.getItem('token');
     if (!token) {
@@ -22,9 +28,39 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function generateTrainingFields(trainings, containerId) {
     trainings.forEach(training => {
-        // Create control flow for trainings
         const singleTrainingContainer = document.createElement('div');
         singleTrainingContainer.setAttribute('class', 'single-training');
+        
+        // Workout type of single training
+        //const singleTrainingTitle = document.createElement('h2');
+        //singleTrainingTitle.innerText = training.workoutType;
+
+        // Create the structure of single training table
+        const singleTrainingPlan = document.createElement('table');
+        const singleTrainingHeaders = document.createElement('tr');
+        Object.values(workoutPlanTableHeaders).forEach(header => {
+            const singleWorkoutPlanTableHeader = document.createElement('th');
+            singleWorkoutPlanTableHeader.innerText = header;
+            singleTrainingHeaders.appendChild(singleWorkoutPlanTableHeader);
+        });
+        singleTrainingPlan.appendChild(singleTrainingHeaders);
+
+
+        // Fill the structure of single training table - Workout plan
+        training.workoutPlan.forEach(exercise => {
+            const singleExerciseRow = document.createElement('tr');
+            Object.keys(workoutPlanTableHeaders).forEach(header => {
+                const singleExerciseCell = document.createElement('td');
+                singleExerciseCell.innerText = exercise[header];
+                singleExerciseRow.appendChild(singleExerciseCell);
+            });
+            singleTrainingPlan.appendChild(singleExerciseRow);
+        });
+
+        //singleTrainingContainer.appendChild(singleTrainingTitle);
+        singleTrainingContainer.appendChild(singleTrainingPlan);
+        document.getElementById(containerId).appendChild(singleTrainingContainer);
+        document.getElementById(containerId).appendChild(document.createElement('br'));
     });
 }
 
@@ -37,31 +73,125 @@ function toggleModal(modalSwitch) {
         document.getElementById('create-training-modal').style.display = "none";
     }
 }
+/* 
+function addTraining() {
+    const token = localStorage.getItem('token');
+    if (!token) {
+        window.location.href = '/';
+        return;
+    }
 
-function addExercise() {
     const workoutPlanTable = document.getElementById('workoutPlan');
-    const workoutPlanTableHeaders = ['Name', 'WeightLoad', 'Sets', 'Repetitions'];
+    const numberOfRows = workoutPlanTable.getElementsByTagName('tr').length;
+
+    if (workoutPlanTable) {
+        const rows = workoutPlanTable.rows;
+
+        console.log(workoutPlanTable.getElementsByTagName('tr')[1].children)
+    }
+
+    toggleModal(false);
+} */
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('trainingForm');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const formData = new FormData(form);
+        const data = {}
+        formData.forEach((value, key) => {
+            data[key] = value;
+        });
+
+        console.log(data, "CCC")
+    })
+});
+/* PRZEROBIC ADDTRAINING WYZEJ NA LISTENERA KTORY WEZMIE Z FORMULARZA DANE, PRZENIESC SUBMIT DO FORMA */
+
+function addExercise() {    
+    const token = localStorage.getItem('token');
+    if (!token) {
+        window.location.href = '/';
+        return;
+    }
+
+    const workoutPlanTable = document.getElementById('workoutPlan');
     const numberOfRows = workoutPlanTable.getElementsByTagName('tr').length;
     const MAX_EXERCISE_NUMBER = 8;
 
     if (numberOfRows <= MAX_EXERCISE_NUMBER) {
         const newRow = document.createElement('tr');
 
-        workoutPlanTableHeaders.forEach(header => {
+        Object.keys(workoutPlanTableHeaders).forEach(header => {
             const newCell = document.createElement('td');
-            const newInput = document.createElement('input');
-            
-            newInput.setAttribute('type', 'text');
-            newInput.setAttribute('required', true);
-            newInput.setAttribute('id', header + "_" + String(numberOfRows));
-            newInput.setAttribute('list', 'exercises-list');
+            let newInput = "";
 
+            if (header == "name") {
+                newInput = generateUserExerciseList(token);
+            } else {
+                newInput = document.createElement('input');
+                
+                newInput.setAttribute('type', 'text');
+                newInput.setAttribute('required', true);
+                newInput.setAttribute('id', workoutPlanTableHeaders[header] + "_" + String(numberOfRows));
+                newInput.setAttribute('list', 'exercises-list');
+            }
             newCell.appendChild(newInput);
             newRow.appendChild(newCell);
         });
 
         workoutPlanTable.appendChild(newRow);
     } else {
-
+        // TODO: Add modal informing about exceeding the MAX EXERCISE NUMBER
     }
+}
+
+function generateUserExerciseList(token) {
+    const newSelect = document.createElement('select');
+    newSelect.className = "exercise-selector"
+
+    // Add private exercises list
+    fetch('/api/get/privateExercise', {
+        method: 'GET',
+        headers: {
+            'Authorization' : `Bearer ${token}`,
+            'Content-Type': 'application/json',
+        },
+    })
+    .then(response => response.json())
+    .then(privateList => {
+        privateList.privateExercise.forEach(singlePrivateExercise => {
+            const newOption = document.createElement('option');
+            newOption.value = singlePrivateExercise.name;
+            newOption.innerText = singlePrivateExercise.name;
+            newSelect.appendChild(newOption);
+        });
+    })
+    .catch(error => {
+        console.log("A private exercise list was not detected:\n", error)
+    });
+
+    // Add global exercises list
+    fetch('/api/get/globalExercise', {
+        method: 'GET',
+        headers: {
+            'Authorization' : `Bearer ${token}`,
+            'Content-Type': 'application/json',
+        },
+    })
+    .then(response => response.json())
+    .then(globalList => {
+        globalList.globalExercise.forEach(singleGlobalExercise => {
+            const newOption = document.createElement('option');
+            newOption.value = singleGlobalExercise.name;
+            newOption.innerText = singleGlobalExercise.name;
+            newSelect.appendChild(newOption);
+        });
+    })
+    .catch(error => {
+        console.log("A global exercise list was not detected:\n", error)
+    });
+
+    return newSelect;
 }
